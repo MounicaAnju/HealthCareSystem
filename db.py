4e4bbfa111bc36b7520b1858d8e12bd5e3e936ef
MounicaAnju <mounicapothureddy555@gmail.com>
2023-04-26 22:30:19 -0400

    remove db

--- db.py
+++
@@ -1,9 +0,0 @@
-import sqlite3
-
-conn = sqlite3.connect('database.db')
-print ("Opened database successfully");
-
-# conn.execute('CREATE TABLE IF NOT EXISTS USERS(fname TEXT, lname TEXT)')
-conn.execute("drop table users")
-print ("Table created successfully");
-conn.close()
